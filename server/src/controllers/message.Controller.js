25a77554356606df2af1be048d6f070af24211c6
Nirmal Kumar G <126151898+NirmalKumar12345@users.noreply.github.com>
2026-07-17 23:15:11 +0530

    message get api implemented

--- server/src/controllers/message.Controller.js
+++ server/src/controllers/message.Controller.js
@@ -30,3 +30,37 @@ export const sendMessage = async (req,res,next)=>{
     }
     
 }
+
+export const getMessages = async(req,res,next)=>{
+    try{
+     const page = Number(req.query.page) || 1;
+     const limit = 20;  
+     const conversation = await Conversation.findById(req.params.conversationId);
+     if(!conversation){
+        return res.status(404).json({
+            success: false,
+            message: "Conversation not found"
+        })
+     }
+     const isParticipants = conversation.participants.some((participant)=>participant.toString()===req.user._id.toString());
+     if(!isParticipants){
+        return res.status(403).json({
+            success: false,
+            message: "Unauthorized to access this conversation"
+        })
+     }
+     const messages = await Message.find({conversation: req.params.conversationId}).populate('sender','name profilePic').sort({createdAt:1}).skip((page-1)*limit).limit(limit);
+     const totalMessages = await Message.countDocuments({conversation: req.params.conversationId});
+     return res.status(200).json({
+        success: true,
+        page,
+        limit,
+        totalMessages,
+        totalPages: Math.ceil(totalMessages / limit),
+        messages
+     })
+
+    }catch(error){
+        next(error)
+    }
+}
--- server/src/routes/message.router.js
+++ server/src/routes/message.router.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import protect from '../middleware/auth.middleware.js';
-import { sendMessage } from '../controllers/message.Controller.js';
+import { sendMessage ,getMessages} from '../controllers/message.Controller.js';
  const router = express.Router();
 
  router.post("/",protect,sendMessage);
+ router.get('/:conversationId',protect,getMessages);
 
  export default router;
